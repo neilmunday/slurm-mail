c9db087d64b9eb03eb705011cc33a005e63d1325
Neil Munday <neilmunday@users.noreply.github.com>
2022-07-25 21:05:38 +0100

    Linter fixes for setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,30 @@
+#
+#  This file is part of Slurm-Mail.
+#
+#  Slurm-Mail is a drop in replacement for Slurm's e-mails to give users
+#  much more information about their jobs compared to the standard Slurm
+#  e-mails.
+#
+#   Copyright (C) 2018-2022 Neil Munday (neil@mundayweb.com)
+#
+#  Slurm-Mail is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at
+#  your option) any later version.
+#
+#  Slurm-Mail is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Slurm-Mail.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Slurm-Mail's setup.py
+"""
+
 import setuptools
 setuptools.setup(
     author='Neil Munday',
@@ -32,7 +59,9 @@ setuptools.setup(
         'setuptools'
     ],
     license='GPLv3',
-    long_description='Slurm-Mail is a drop in replacement for Slurm\'s e-mails to give users much more information about their jobs compared to the standard Slurm e-mails.',
+    long_description='Slurm-Mail is a drop in replacement for Slurm\'s e-mails ' +
+    'to give users much more information about their jobs compared to the ' +
+    'standard Slurm e-mails.',
     maintainer='Neil Munday',
     name='slurmmail',
     packages=['slurmmail'],
