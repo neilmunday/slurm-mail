39ac6ed69e862ecca6f9e5a8588385a3874aee8b
Neil Munday <neilmunday@users.noreply.github.com>
2022-08-01 23:02:38 +0100

    Added DEFAULT_DATETIME_FORMAT constant

--- slurmmail/__init__.py
+++ slurmmail/__init__.py
@@ -7,3 +7,6 @@ import pathlib
 conf_dir = pathlib.Path("/etc/slurm-mail")
 conf_file = conf_dir / "slurm-mail.conf"
 tpl_dir = conf_dir / "templates"
+
+# defaults
+DEFAULT_DATETIME_FORMAT = "%d/%m/%Y %H:%M:%S"
