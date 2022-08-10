ada2142e45396079737b920518215b291af1189b
Neil Munday <neilmunday@users.noreply.github.com>
2022-08-10 21:44:23 +0100

    Added package properties to __init__.py for use by other utilities

--- slurmmail/__init__.py
+++ slurmmail/__init__.py
@@ -10,3 +10,14 @@ tpl_dir = conf_dir / "templates"
 
 # defaults
 DEFAULT_DATETIME_FORMAT = "%d/%m/%Y %H:%M:%S"
+
+# properties
+ARCHITECTURE = 'any'
+EMAIL = 'neilmunday@users.noreply.github.com'
+DESCRIPTION = 'Provides enhanced e-mails for Slurm.'
+LONG_DESCRIPTION = 'Slurm-Mail is a drop in replacement for Slurm\'s e-mails ' + \
+    'to give users much more information about their jobs compared to the ' + \
+    'standard Slurm e-mails.'
+MAINTAINER = 'Neil Munday'
+NAME = 'slurmmail'
+VERSION = '4.0'
