650e013dda241f98c1b0ed21b538956748158e5d
Neil Munday <neilmunday@users.noreply.github.com>
2023-11-24 11:36:23 +0000

    fix: corrected Slurm-Mail version

--- src/slurmmail/__init__.py
+++ src/slurmmail/__init__.py
@@ -43,5 +43,5 @@ LONG_DESCRIPTION = 'Slurm-Mail is a drop in replacement for Slurm\'s ' + \
     ' compared to the standard Slurm e-mails.'
 MAINTAINER = 'Neil Munday'
 NAME = 'slurmmail'
-VERSION = '4.6'
+VERSION = '4.7'
 URL = 'https://www.github.com/neilmunday/slurm-mail'
