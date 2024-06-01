e799220fdf2a18a8bc28c46df506f5c25260172d
Neil Munday <neilmunday@users.noreply.github.com>
2024-06-01 11:43:30 +0100

    docs: Added copyright info

--- tests/integration/docker-slurm/new-version.py
+++ tests/integration/docker-slurm/new-version.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
-# pylint: disable=duplicate-code,invalid-name,missing-module-docstring
+# pylint: disable=duplicate-code,invalid-name,missing-module-
+
+#
+#  This file is part of Slurm-Mail.
+#
+#  Slurm-Mail is a drop in replacement for Slurm's e-mails to give users
+#  much more information about their jobs compared to the standard Slurm
+#  e-mails.
+#
+#   Copyright (C) 2018-2024 Neil Munday (neil@mundayweb.com)
+#
+#  Slurm-Mail is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at
+#  your option) any later version.
+#
+#  Slurm-Mail is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Slurm-Mail.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import argparse
 import fileinput
